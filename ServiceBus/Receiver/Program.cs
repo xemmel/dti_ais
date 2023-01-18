6d45cea603f4d2634133ba9c05c82e9e9f9cf2ab
Admin <Admin@TA-8214-INS.kursdom.net>
2023-01-18 14:59:31 +0100

    Added ServiceBus code

---
+++ ServiceBus/Receiver/Program.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+
+var credential = new AzureCliCredential();
+var serviceBusName = "teknoservicebus01";
+var client = new ServiceBusClient(
+    fullyQualifiedNamespace: $"{serviceBusName}.servicebus.windows.net",
+    credential: credential);
+
+
+var receiver = client.CreateReceiver(
+    topicName: "mytopic",
+    subscriptionName: "allmessagesfromdenmark",
+    options: new ServiceBusReceiverOptions { ReceiveMode = ServiceBusReceiveMode.ReceiveAndDelete });
+var messages = await receiver.ReceiveMessagesAsync(10, maxWaitTime: new TimeSpan(0, 0, 40));
+foreach (var message in messages)
+{
+    var messageBody = message.Body.ToString();
+    System.Console.WriteLine($"Message received: {messageBody}");
+}
+
+
---
+++ ServiceBus/Submitter/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+
+var credential = new AzureCliCredential();
+var serviceBusName = "teknoservicebus01";
+var client = new ServiceBusClient(
+    fullyQualifiedNamespace : $"{serviceBusName}.servicebus.windows.net",
+    credential: credential);
+string country = args[0];
+var sender = client.CreateSender("mytopic");
+
+var message = new ServiceBusMessage("hello from application");
+message.ApplicationProperties["country"] = country;
+await sender.SendMessageAsync(message);
+System.Console.WriteLine("Message sent...");
