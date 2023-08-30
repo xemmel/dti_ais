92ec9f2e1e4d19ba5457b0a9ffe66c0c8cc1799e
Morten la Cour <lacour@gmail.com>
2023-08-30 14:38:33 +0200

    service bus code

---
+++ Courses/204_202308/code/servicebusconsumer/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+
+Console.WriteLine("Hello, World!");
+
+
+string serviceBusNamespace = "the204sbnamespace";
+string topicName = "codedemo";
+string subscription = args[0];
+
+var credential = new AzureCliCredential();
+
+ServiceBusClient client = new ServiceBusClient(
+        fullyQualifiedNamespace: $"{serviceBusNamespace}.servicebus.windows.net",
+        credential: credential);
+
+var receiver = client.CreateReceiver(
+    topicName: topicName, subscriptionName: subscription,
+    options: new ServiceBusReceiverOptions { ReceiveMode = ServiceBusReceiveMode.ReceiveAndDelete });
+await foreach (var message in receiver.ReceiveMessagesAsync())
+{
+    var messageString = message.Body.ToString();
+    System.Console.WriteLine(messageString);
+}
+
---
+++ Courses/204_202308/code/servicebussubmitter/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+
+Console.WriteLine("Hello, World!");
+
+string serviceBusNamespace = "the204sbnamespace";
+string topicName = "codedemo";
+
+string country = args[0];
+
+var credential = new AzureCliCredential();
+
+ServiceBusClient client = new ServiceBusClient(
+        fullyQualifiedNamespace: $"{serviceBusNamespace}.servicebus.windows.net",
+        credential: credential);
+
+var sender = client.CreateSender(topicName);
+
+var message = new ServiceBusMessage("I am a message from a .NET app");
+message.ApplicationProperties.Add("country",country);
+
+await sender.SendMessageAsync(message);
+
+
+System.Console.WriteLine("Message sent");
+
